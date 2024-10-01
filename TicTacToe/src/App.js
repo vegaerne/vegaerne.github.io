62ae9aa99d33c250747536fede010daae46515be
vegaerne <vegaerne0606@hotmail.com>
2024-10-01 04:14:52 -0400

    add div

--- TicTacToe/src/App.js
+++ TicTacToe/src/App.js
@@ -1,3 +1,21 @@
 export default function Square() {
-  return < button className = "square">X</button>
+  return(
+    <>
+      <div classsName="board-row">
+      <button className = "square">X</button>
+      <button className = "square">X</button>
+      <button className = "square">X</button>
+      </div>
+      <div classsName="board-row">
+      <button className = "square">X</button>
+      <button className = "square">X</button>
+      <button className = "square">X</button>
+      </div>
+      <div classsName="board-row">
+      <button className = "square">X</button>
+      <button className = "square">X</button>
+      <button className = "square">X</button>
+      </div>
+    </>
+  );
 }
